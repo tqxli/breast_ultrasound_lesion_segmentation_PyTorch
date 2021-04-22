b9733e9981dc1abd09f3aee7efbbefa89ad74d5d
tqxli <tqxli1527@g.ucla.edu>
2021-04-21 21:26:48 -0700

    Modified data prep script.

--- BUSI/BUSI_prepare_for_training.py
+++ BUSI/BUSI_prepare_for_training.py
@@ -2,11 +2,20 @@ import zipfile
 from google_drive_downloader import GoogleDriveDownloader as gdd
 import os
 
+# gdd.download_file_from_google_drive(file_id='16TpYrPxPK8_nhLmyNax6MLn7aYamMGJt',
+#                                     dest_path='./BUSI/BUSI_train_complete.zip',
+#                                     unzip=True)
+                                    
+# with zipfile.ZipFile('BUSI/BUSI_train_complete.zip', 'r') as zip_ref:
+#     zip_ref.extractall('BUSI')
+
+# os.rename('BUSI/BUSI_train_complete', 'BUSI/BUSI_train')
+
 gdd.download_file_from_google_drive(file_id='16TpYrPxPK8_nhLmyNax6MLn7aYamMGJt',
-                                    dest_path='./BUSI/BUSI_train_complete.zip',
+                                    dest_path='./BUSI/BUSI_train_single_mass.zip',
                                     unzip=True)
                                     
-with zipfile.ZipFile('BUSI/BUSI_train_complete.zip', 'r') as zip_ref:
-    zip_ref.extractall('BUSI')
+#with zipfile.ZipFile('BUSI/BUSI_train_single_mass.zip', 'r') as zip_ref:
+#    zip_ref.extractall('BUSI')
 
-os.rename('BUSI/BUSI_train_complete', 'BUSI/BUSI_train')
+os.rename('BUSI/BUSI_train_single_mass', 'BUSI/BUSI_train')
